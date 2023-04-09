bfc7118e77462e731a2be5bd8913ebc27266e1b7
Carlock <angelfacarlock@gmail.com>
2023-04-08 22:57:12 -0600

    Agregue tab5

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,6 +12,10 @@ const routes: Routes = [
   {
     path: 'tab5',
     loadChildren: () => import('./tab5/tab5.module').then( m => m.Tab5PageModule)
+  },
+  {
+    path: 'tab5',
+    loadChildren: () => import('./tab5/tab5.module').then( m => m.Tab5PageModule)
   }
 
 ];
